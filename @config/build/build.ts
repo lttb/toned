be548c6c99becf70fc6b24e110909830c4bc664f
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2025-08-22 11:07:47 +0100

    feat(build): create dist dir

--- @config/build/build.ts
+++ @config/build/build.ts
@@ -2,13 +2,17 @@ import * as path from 'node:path'
 import { $ } from 'bun'
 
 const cwd = process.cwd()
-const dist = path.resolve(cwd, '.dist/')
+const dist = path.resolve(cwd, 'dist/')
 
 const transformPkg = async () => {
   const { scripts, devDependencies, ...pkg } =
     await Bun.file('package.json').json()
 
-  await Bun.write(path.join(dist, 'package.json'), JSON.stringify(pkg, null, 2))
+  await Bun.write(
+    path.join(dist, 'package.json'),
+    JSON.stringify(pkg, null, 2),
+    { createPath: true },
+  )
 }
 
 await $`rm -rf ${dist}`
