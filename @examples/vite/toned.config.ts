d03a482b3bc6293de7d645a426d6dbdd8695c505
Artur Kenzhaev <kenzhaev.artur@gmail.com>
2025-09-16 15:27:46 +0100

    feat(examples/vite): restructure toned.config

--- @examples/vite/toned.config.ts
+++ @examples/vite/toned.config.ts
@@ -1,6 +1,12 @@
+import '@toned/themes/shadcn/config.css'
+
 import { defineConfig, setConfig } from '@toned/core'
 
+import { insert } from '@toned/core/dom'
 import reactConfig from '@toned/react/new/react-web'
+import { system } from '@toned/systems/base'
+
+insert(system)
 
 // should be preset for the framework?
 // also add options to process css-variables/native/tailwind etc
